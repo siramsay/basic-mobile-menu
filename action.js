37875e7a6d9e82451ed54ea22c3a1234e5aa889a
simon <simon@designkojo.net>
2023-08-08 07:45:32 +0900

    JavaScript: open menu click event listener and function

--- action.js
+++ action.js
@@ -1,1 +1,7 @@
+const menuToggle= document.querySelector('button.mobile-menu-toggle');
+const mobileMenu= document.querySelector('nav.mobile-menu');
+
+menuToggle.addEventListener('click', function () {
+    mobileMenu.classList.add('open');
+})
 
