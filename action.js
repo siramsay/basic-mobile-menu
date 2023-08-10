6733839c2dee0f17d26f99fd2b2f5290af139081
simon <simon@designkojo.net>
2023-08-11 07:13:44 +0900

    add click function conditional for open close, add z-index and cursor to menu button

--- action.js
+++ action.js
@@ -2,6 +2,14 @@ const menuToggle= document.querySelector('button.mobile-menu-toggle');
 const mobileMenu= document.querySelector('nav.mobile-menu');
 
 menuToggle.addEventListener('click', function () {
-    mobileMenu.classList.add('open');
+
+    if(mobileMenu.classList.contains('open')){
+      mobileMenu.classList.remove('open');
+      menuToggle.innerHTML = 'Menu';
+    } else {
+      mobileMenu.classList.add('open');
+      menuToggle.innerHTML = 'Close';
+    }
+
 })
 
