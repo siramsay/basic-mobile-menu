5235923098d7439afef3797a6c6539c8f1d7daf4
simon <simon@designkojo.net>
2023-08-17 17:20:03 +0900

    inert and @supports rules

--- action.js
+++ action.js
@@ -3,13 +3,13 @@ const mobileMenu= document.querySelector('nav.mobile-menu');
 
 menuToggle.addEventListener('click', function () {
 
-    if(mobileMenu.classList.contains('open')){
-      mobileMenu.classList.remove('open');
-      menuToggle.innerHTML = 'Menu';
-    } else {
-      mobileMenu.classList.add('open');
-      menuToggle.innerHTML = 'Close';
-    }
+  if(mobileMenu.classList.contains('open')){
+    mobileMenu.classList.remove('open');
+    menuToggle.innerHTML = 'Menu';
+  } else {
+    mobileMenu.classList.add('open');
+    menuToggle.innerHTML = 'Close';
+  }
 
 })
 
