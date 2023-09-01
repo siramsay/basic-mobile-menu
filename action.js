7bd4f7cadfb1331aff3ed761947aab1ec13503e2
simon <simon@designkojo.net>
2023-09-02 08:11:13 +0900

    add inert support and fallback

--- action.js
+++ action.js
@@ -1,15 +1,17 @@
 const menuToggle= document.querySelector('button.mobile-menu-toggle');
 const mobileMenu= document.querySelector('nav.mobile-menu');
+const mainContent= document.querySelector('main');
 
 menuToggle.addEventListener('click', function () {
 
   if(mobileMenu.classList.contains('open')){
     mobileMenu.classList.remove('open');
     menuToggle.innerHTML = 'Menu';
+    mainContent.removeAttribute('inert');
   } else {
     mobileMenu.classList.add('open');
     menuToggle.innerHTML = 'Close';
+    mainContent.setAttribute('inert', 'true');
   }
-
 })
 
