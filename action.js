ba9c354194d0e17a7c21ceafdbfd8b3847acc784
simon <simon@designkojo.net>
2023-09-02 09:03:49 +0900

    add class to body when menu active, add CSS to disable scroll

--- action.js
+++ action.js
@@ -1,6 +1,7 @@
 const menuToggle= document.querySelector('button.mobile-menu-toggle');
 const mobileMenu= document.querySelector('nav.mobile-menu');
 const mainContent= document.querySelector('main');
+const body = document.querySelector('body');
 
 menuToggle.addEventListener('click', function () {
 
@@ -8,10 +9,12 @@ menuToggle.addEventListener('click', function () {
     mobileMenu.classList.remove('open');
     menuToggle.innerHTML = 'Menu';
     mainContent.removeAttribute('inert');
+    body.classList.remove("mobile-menu-active");
   } else {
     mobileMenu.classList.add('open');
     menuToggle.innerHTML = 'Close';
     mainContent.setAttribute('inert', 'true');
+    body.classList.add("mobile-menu-active");
   }
 })
 
